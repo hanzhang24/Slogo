4d5b3ef2dc2698260e5e9bc7d4fc65d4da627c9f
Han Zhang <hz244@duke.edu>
2023-02-23 15:20:32 -0500

    Start of View Back

---
+++ src/main/java/View/Screen.java
@@ -0,0 +1,17 @@
+package View;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.Group;
+
+public abstract class Screen {
+
+  Pane root;
+  Scene scene;
+
+  public Scene makeScene(){
+    Group root = new Group();
+    Scene newScene = Scene(root, )
+    return Scene;
+  }
+}
