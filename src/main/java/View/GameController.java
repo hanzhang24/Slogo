5259d0064f852e724663e4fbb28285476add78e4
Aryan Kothari <ak616@duke.edu>
2023-02-23 18:06:21 -0500

    made GameController Class

---
+++ src/main/java/View/GameController.java
@@ -0,0 +1,23 @@
+package View;
+
+import java.awt.Dimension;
+
+import View.Screens.SplashScreen;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class GameController extends Application {
+    public static final String TITLE = "Slogo Group 7";
+    public static final Dimension DEFAULT_SIZE = new Dimension(1000, 1000);
+    public static final String DEFAULT_LANGUAGE = "English";
+
+
+    @Override
+    public void start (Stage stage) {
+        Screen SplashScreen = new SplashScreen(stage);
+        stage.setScene(SplashScreen.makeScene(DEFAULT_SIZE.width, DEFAULT_SIZE.height));
+        stage.setTitle(TITLE);
+        stage.show();
+    }
+}
