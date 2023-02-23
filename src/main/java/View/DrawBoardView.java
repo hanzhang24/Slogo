1d69192bdc352acb46d73832d02c2c55cf20267e
Han Zhang <hz244@duke.edu>
2023-02-23 18:11:07 -0500

    Added DrawBoardView.java basic constructor

--- src/main/java/View/DrawBoardView.java
+++ src/main/java/View/DrawBoardView.java
@@ -1,5 +1,18 @@
 package View;
 
-public class DrawBoardView {
+import javafx.scene.Node;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 
+public class DrawBoardView {
+  Canvas canvas;
+  public DrawBoardView(){
+    //TODO move into actual class
+    canvas = new Canvas(100, 100);
+    canvas.setId("Canvas");
+    GraphicsContext gc = canvas.getGraphicsContext2D();
+  }
+  public Node getCanvas() {
+    return canvas;
+  }
 }
