10c3c62cf92e9e947a6bcb255468bbb98c40cf95
Han Zhang <hz244@duke.edu>
2023-02-23 18:11:39 -0500

    Added width and height to resource bundle

--- src/main/java/View/Screens/GameScreen.java
+++ src/main/java/View/Screens/GameScreen.java
@@ -1,13 +1,25 @@
 package View.Screens;
 
+import View.DrawBoardView;
 import View.Screen;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 public class GameScreen extends Screen {
 
-
+  public static final String STYLESHEET = "default.css";
+  public GameScreen(Stage primaryStage){
+    this.root = new GridPane();
+    this.stage = primaryStage;
+  }
   @Override
   public Scene makeScene() {
-    return null;
+    DrawBoardView canvas = new DrawBoardView();
+    this.root.getChildren().add(canvas.getCanvas());
+    Group Root = new Group();
+    Scene primaryScene = new Scene(Root, Double.parseDouble(this.resources.getString("Width")), Double.parseDouble(this.resources.getString("Height")));
+    primaryScene.getStylesheets().add(STYLESHEET);
+    return primaryScene;
   }
 }
