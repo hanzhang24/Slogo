6c8591d0bdacb8c1e2c68280bbd708ef89aa92ab
Han Zhang <hz244@duke.edu>
2023-02-26 00:34:21 -0500

    Added two units tests for SplashScreenTest, average line coverage of 95% for each view class.

---
+++ src/test/java/View/Screens/SplashScreenTest.java
@@ -0,0 +1,59 @@
+package View.Screens;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.Dimension;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ComboBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import util.DukeApplicationTest;
+
+class SplashScreenTest extends DukeApplicationTest {
+  private static final String TITLE = "SLogo Team 6";
+  private static final Dimension DEFAULT_SIZE = new Dimension(1000, 1000);
+  private static final String DEFAULT_LANGUAGE = "English";
+  private static final ObservableList<String> LANGUAGE_OPTIONS  =
+      FXCollections.observableArrayList(
+          "English",
+          "Spanish"
+      );
+
+  private SplashScreen thisScreen;
+  private ComboBox languages;
+  private Node button;
+  private ColorPicker color;
+
+  @Override
+  public void start (Stage stage) {
+    thisScreen = new SplashScreen(stage, DEFAULT_LANGUAGE, LANGUAGE_OPTIONS);
+    stage.setScene(thisScreen.makeScene(DEFAULT_SIZE.width, DEFAULT_SIZE.height));
+    stage.setTitle(TITLE);
+    stage.show();
+
+    languages = lookup("#Language-Box").query();
+    button = lookup("#Go-Button").query();
+    color = lookup("#Color-Selector").query();
+  }
+
+  @Test
+  void testValidLanguage() {
+    select(languages, "english");
+    clickOn(button);
+    assertEquals("english", thisScreen.getChosenLanguage());
+  }
+
+  @Test
+  void testValidColor() {
+    Color selected = Color.BLUE;
+    setValue(color, selected);
+    select(languages, "english");
+    clickOn(button);
+    assertEquals(selected, thisScreen.getChosenColor());
+  }
+}
