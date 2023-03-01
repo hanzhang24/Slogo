9b76a391cf4a1f31a3906b6ddb83100ed367fb08
Alec Liu <alec.liu@duke.edu>
2023-02-28 23:44:54 -0500

    Changed Model API to allow for history and return values. Note that ParserTest.java and Controller.java need to be modified (endOp changed) to fully build the app

--- src/main/java/slogo/Model/History.java
+++ src/main/java/slogo/Model/History.java
@@ -1,38 +1,38 @@
-//package slogo.Model;
-//
-//import java.util.Stack;
-//
-///**
-// * @author Alec Liu The History class is meant to track the history of user commands. Note: probably
-// * should just live in the View section as a separate model
-// */
-//public class History {
-//
-//  private Stack<String> commandLog;
-//
-//  /**
-//   * Class constructor
-//   */
-//  public History() {
-//    commandLog = new Stack<>();
-//  }
-//
-//  /**
-//   * Log a new entry into the history
-//   *
-//   * @param userInput user input
-//   */
-//  public void addEntry(String userInput) {
-//    commandLog.push(userInput);
-//  }
-//
-//  /**
-//   * Fetch the most recent command. Should only called when endOp() is called, representing a
-//   * successful operation.
-//   *
-//   * @return user's last command
-//   */
-//  public String getLastCommand() {
-//    return commandLog.peek();
-//  }
-//}
+package slogo.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Alec Liu The History class is meant to track the history of user commands. Note: probably
+ * should just live in the View section as a separate model
+ */
+public class History {
+
+  private List<String> commandLog;
+
+  /**
+   * Class constructor
+   */
+  public History() {
+    commandLog = new ArrayList<>();
+  }
+
+  /**
+   * Log a new entry into the history
+   *
+   * @param userInput user input
+   */
+  public void addEntry(String userInput) {
+    commandLog.add(userInput);
+  }
+
+  /**
+   * Fetch all previous history for this current application
+   *
+   * @return user command history
+   */
+  List<String> getAllHistory() {
+    return commandLog;
+  }
+}
