254a3b1c1f2a64520a2da73d615c5513236d0710
Han Zhang <hz244@duke.edu>
2023-03-04 14:27:57 -0500

    Changed the Animation to be inside of Animation and updated SliderView Class

--- src/main/java/slogo/View/Animator.java
+++ src/main/java/slogo/View/Animator.java
@@ -11,7 +11,12 @@ import javafx.util.Duration;
 public class Animator {
   private Animation action;
   private PenView avatar;
+  private double animationSpeed;
+
+  public static final int FRAMES_PER_SECOND = 60;
+  public static final int DEFAULT_SPEED = 60;
   public Animator(PenView gameAvatar){
+    animationSpeed = DEFAULT_SPEED;
     avatar = gameAvatar;
   }
   // create sequence of animations
@@ -20,8 +25,13 @@ public class Animator {
     path.getElements().addAll(new MoveTo(avatar.getXCor() + 25, avatar.getYCor() + 25),
         new LineTo(endX + 300, endY + 300));
     avatar.updatePosXY(endX, endY);
-    PathTransition pt = new PathTransition(Duration.seconds(2), path, avatar.getImage());
+    PathTransition pt = new PathTransition(Duration.seconds(FRAMES_PER_SECOND/animationSpeed), path, avatar.getImage());
     return new SequentialTransition(avatar.getImage(), pt);
   }
-
+  public double getAnimationSpeed() {
+    return animationSpeed;
+  }
+  public void setAnimationSpeed(double speed){
+    animationSpeed = speed;
+  }
 }
--- src/main/java/slogo/View/Containers/SliderView.java
+++ src/main/java/slogo/View/Containers/SliderView.java
@@ -1,24 +1,58 @@
 package slogo.View.Containers;
 
+import java.util.ResourceBundle;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import slogo.View.Containers.ContainerView;
+import slogo.View.Animator;
 
 public class SliderView extends ContainerView {
 
-  Slider slider;
+  private Slider slider;
 
-  public SliderView(){
-    slider = new Slider();
-    slider.setId("Animation-Slider");
+  private TextArea area;
+  private ResourceBundle sliderBundle;
+  private static final String DEFAULT_RESOURCE_PACKAGE = "View.";
+  private static final String DEFAULT_RESOURCE_FOLDER = "/"+DEFAULT_RESOURCE_PACKAGE.replace(".", "/");
+  private static final String STYLESHEET = "SliderContainer.css";
+  private static final String SLIDER_RESOURCES = "SliderBoxParams";
+
+  public SliderView(Animator animations){
+    sliderBundle = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + SLIDER_RESOURCES);
     HBox container = new HBox();
     this.setContainer(container);
     container.setId("Animation-Box");
-    
+    setUpSlider(animations, container);
+    container.getStylesheets().add(getClass().getResource(DEFAULT_RESOURCE_FOLDER + STYLESHEET).toExternalForm());
+
+    setUpTextArea(container);
   }
 
-  public Slider getSlider() {
-    return slider;
+  private void setUpTextArea(HBox container) {
+    area = new TextArea();
+    area.setId("Animation-Input");
+    container.getChildren().add(area);
+  }
+
+  private void setUpSlider(Animator animations, HBox container) {
+    slider = new Slider();
+    slider.setId("Animation-Slider");
+    slider.setOnMouseReleased(e -> setAnimationSpeed(animations));
+    slider.setMin(Double.parseDouble(sliderBundle.getString("Min")));
+    slider.setMax(Double.parseDouble(sliderBundle.getString("Max")));
+    slider.setValue(Double.parseDouble(sliderBundle.getString("Default")));
+    slider.setBlockIncrement(Double.parseDouble(sliderBundle.getString("Increment")));
+    slider.setShowTickLabels(Boolean.parseBoolean(sliderBundle.getString("ShowLabels")));
+    slider.setShowTickMarks(Boolean.parseBoolean(sliderBundle.getString("ShowTicks")));
+    container.getChildren().add(slider);
+    animations.setAnimationSpeed(slider.getValue());
+  }
+
+  public void setAnimationSpeed(Animator animations){
+    animations.setAnimationSpeed(slider.getValue());
+  }
+  public Pane getSliderContainer(){
+    return this.getContainer();
   }
 }
