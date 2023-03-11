2e8c5ea745baab09b4c3377caa1b113047c1da9e
Alec Liu <alec.liu@duke.edu>
2023-03-11 00:09:23 -0500

    Added button for multiple workspaces

--- src/main/java/slogo/View/Screens/GameScreen.java
+++ src/main/java/slogo/View/Screens/GameScreen.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceDialog;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -23,6 +24,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import slogo.Main;
 import slogo.Parser.XMLParser;
 import slogo.View.Animator;
 import slogo.View.Containers.HistoryView;
@@ -455,5 +457,10 @@ public class GameScreen extends Screen implements ModelView {
         xmlParser.getType());
     alert.setContentText(body);
     return alert;
+  }
+
+  public void openNewWindow(){
+    Main main = new Main();
+    main.start(new Stage());
   }
 }
